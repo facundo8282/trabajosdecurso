d6d84b5c1c46d051c52864ea908b17526dffc914
facundo8282 <80721945+facundo8282@users.noreply.github.com>
2023-03-01 17:19:01 -0300

    ejercicio2

---
+++ ejercicio2.java
@@ -0,0 +1,23 @@
+package ejercicio2;
+
+public class ejercicio2 {
+	public static void main(String args[]) {
+		float ingresos = 299000;
+		int superficie = 40;
+		int energia = 4000;
+		if ( (ingresos <= 748382.07) &&
+				( superficie <= 30) &&
+				(energia <= 3330) ) {
+				System.out.println("Categoría A");
+				} else if((ingresos <= 1112459.83) &&
+				(superficie <= 45) &&
+				(energia <= 5000) ) {
+				System.out.println("Categoría B");
+				}
+
+		
+
+	}
+
+
+}
